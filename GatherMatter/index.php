0d2a2efc3dc494d59bc20cf8d2efcfbf1c24da00
gr01b-bekmez <bekmez-emirhan@outlook.com>
2023-05-23 09:21:04 +0200

    Implementierung Responsive Design

--- GatherMatter/index.php
+++ GatherMatter/index.php
@@ -14,9 +14,9 @@
     ?>
 
     <!-- Content -->
-    <div style="display: flex; flex-direction: row;">
-    <h1>Lorem ipsum</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic accusamus consectetur aperiam reiciendis facere necessitatibus quia cupiditate commodi? Ea quod nesciunt dolorem possimus! Sunt, modi soluta. Eaque laborum nostrum temporibus?</p>
+    <div style="display: flex; flex-direction: column; margin: 20px;">
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quia sit sapiente reprehenderit, repellendus qui quod ab molestias vitae minima ex ducimus dicta ullam molestiae cupiditate rem dolor tenetur debitis, exercitationem unde voluptates at nihil? Itaque, magnam voluptatum ducimus eligendi sunt aspernatur accusamus obcaecati. Eum doloribus nisi corrupti, illo quasi laudantium quaerat dolores molestias quibusdam ad excepturi eius possimus alias minima adipisci enim doloremque quo facere. Distinctio earum reiciendis expedita delectus esse iusto voluptatem odio maxime quibusdam porro laborum neque sit similique, ad quas, quia blanditiis hic! Facere molestiae qui fugiat libero aliquam asperiores eligendi vel possimus optio. Placeat, eius?<p>
     </div>
 
     <!-- Footer -->
--- GatherMatter/sites/header.php
+++ GatherMatter/sites/header.php
@@ -1,29 +1,55 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top" >
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">
             <img src="./src\assets\logo-black.png" alt="logo" width="60px" height="60px">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-            <ul class="navbar-nav" style="padding-left: 80px;">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-              </li>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
             </ul>
-          </div>
-          <div class="d-flex">
-           <!--  <ul class="navbar-nav">
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+           <form class="d-flex mt-2 mt-lg-0" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="height: 30px;">
+                <button class="btn btn-sm btn-primary" style="height: 30px; margin-right: 20px;" type="submit">Search</button>
+            </form> 
+            <div class="d-flex">
+            <button type="button" class="btn btn-sm btn-primary" style="margin-right: 5px;" >Register</button>
+            <button type="button" class="btn btn-sm btn-secondary">Login</button>
+            </div>
+        
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!--  <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link" href="#">Register</a>
               </li>
@@ -31,10 +57,3 @@
                 <a class="nav-link" href="#">Einloggen</a>
               </li> 
             </ul>-->
-          </div>
-          <div class="text-end">
-              <button type="button" class="btn btn-sm btn-primary" disabled>Register</button>
-              <button type="button" class="btn btn-sm btn-secondary" disabled>Login</button>
-          </div>
-        </div>
-      </nav>
