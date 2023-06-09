bfd5f6cbc2b1ec089b05746668a6f536786410c5
gr01a-bauer <lbs@muri.io>
2023-06-09 14:17:09 +0200

    ich migriere mich bald selber

--- GatherMatter/database/migrations/2023_06_09_121612_create_events_table.php
+++ GatherMatter/database/migrations/2023_06_09_121612_create_events_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->integer('maxParticipants')->nullable();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->default('0000-00-00 00:00:00');
-            $table->text('image_path')->nullable();
+            $table->text('image_path')->default('images/events/no_img.jpg');
         });
     }
 
