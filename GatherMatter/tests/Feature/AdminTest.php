c9b5489ce577f85d7e870907a6a894d7cd9a86f4
gr01a-bauer <lbs@muri.io>
2023-06-12 04:56:27 +0000

    automatisierte Tests überarbeitet

---
+++ GatherMatter/tests/Feature/AdminTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class AdminTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_User_mit_Adminrolle_kann_erstellt_werden()
+    {
+        // Erstelle einen Admin-Benutzer
+        $admin = User::factory()->create(['role' => 'admin']);
+   
+	// Überprüfe, ob der Benutzer in der Datenbank existiert
+   	$this->assertDatabaseHas('users', [
+        	'email' => $admin->email,
+    	]);
+
+    }
+
+    public function test_Admin_kann_sich_einloggen()
+    {
+        // Erstelle einen Admin-Benutzer
+        $admin = User::factory()->create(['role' => 'admin']);
+
+	// Versuche dich anzumelden
+        $response = $this->post('/login', [
+            'email' => $admin->email,
+            'password' => 'password'
+        ]);
+
+        // Stelle sicher, dass der Admin authentifiziert wurde
+        $response->assertRedirect('/home');
+    }
+
+    public function test_Admin_kommt_ins_Adminpanel_rein()
+    {
+        // Erstelle einen Admin-Benutzer
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        // Melde den Admin an
+        $this->be($admin);
+
+        // Versuche auf das Admin-Panel zuzugreifen
+        $response = $this->get('/admin');
+
+        // Stelle sicher, dass der Admin auf das Admin-Panel zugreifen kann
+        $response->assertStatus(200);
+    }
+}
--- GatherMatter/tests/Feature/EventTest.php
+++ GatherMatter/tests/Feature/EventTest.php
@@ -11,23 +11,23 @@ class EventTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
-    public function an_event_can_be_created()
+    public function test_Event_kann_erstellt_werden()
     {
         $event = Event::factory()->create();
 
+	// Stellt sicher dass das Testevent in der Datenbank vorhanden ist
         $this->assertDatabaseHas('events', [
             'title' => $event->title
         ]);
     }
 
-    /** @test */
-    public function an_event_can_be_deleted()
+    public function test_Event_kann_geloescht_werden()
     {
         $event = Event::factory()->create();
 
         $event->delete();
 
+	// Stellt sicher dass das Testevent nicht mehr in der Datenbank ist
         $this->assertDatabaseMissing('events', [
             'title' => $event->title
         ]);
