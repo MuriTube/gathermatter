9c5aa0027eceb3c4dd9757632711b67bbae68cf9
gr01a-bauer <lbs@muri.io>
2023-06-13 18:53:28 +0200

    cart funktioniert 1.0

--- GatherMatter/resources/views/cart.blade.php
+++ GatherMatter/resources/views/cart.blade.php
@@ -1,27 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @if (session('success'))
-                <div class="alert alert-success mt-4">
-                    {{ session('success') }}
-                </div>
-            @endif
-            @if(session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-            @endif
-            <div class="card shadow-lg">
-                <div class="card-body">
-                    <h5 class="card-title">Shopping Cart</h5>
-                    @if(count($cartItems) > 0)
-                        <ul class="list-group">
-                            @foreach($cartItems as $item)
-                                <div class="card mb-3">
-                                    <div class="card-body">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @if (session('success'))
+                    <div class="alert alert-success mt-4">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if(session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title">Shopping Cart</h5>
+                        @if(count($cartItems) > 0)
+                            <div class="list-group">
+                                @foreach($cartItems as $item)
+                                    <div class="list-group-item mb-3 p-3 shadow">
                                         @if($item->ticket && $item->ticket->event)
                                             <p>Event: {{ $item->ticket->event->title }}</p>
                                             <p>Ticket: {{ $item->ticket->tier }}</p>
@@ -30,31 +29,31 @@
                                         @else
                                             <p>Error: Ticket or event not found for this cart item.</p>
                                         @endif
-                                    <div class="text-end">
-                                        <form action="{{ route('cart.update', $item) }}" method="POST" class="d-inline">
-                                            @csrf
-                                            @method('PUT')
-                                            <input type="number" name="quantity" value="{{ $item->quantity }}" min="1" max="10" class="form-control d-inline" style="width: 70px;">
-                                            <button type="submit" class="btn btn-primary">Update</button>
-                                        </form>
-                                        <form action="{{ route('cart.delete', $item) }}" method="POST" class="d-inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Remove</button>
-                                        </form>
+                                        <div class="text-end">
+                                            <form action="{{ route('cart.update', $item) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('PUT')
+                                                <input type="number" name="quantity" value="{{ $item->quantity }}" min="1" max="10" class="form-control d-inline" style="width: 70px;">
+                                                <button type="submit" class="btn btn-primary">Update</button>
+                                            </form>
+                                            <form action="{{ route('cart.delete', $item) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Remove</button>
+                                            </form>
+                                        </div>
                                     </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                        <div class="text-end mt-3">
-                            <a href="{{ route('payment.initiate') }}" class="btn btn-success">Proceed to Payment</a>
-                        </div>
-                    @else
-                        <p>Your cart is empty.</p>
-                    @endif
+                                @endforeach
+                            </div>
+                            <div class="text-end mt-3">
+                                <a href="{{ route('payment.initiate') }}" class="btn btn-success">Proceed to Payment</a>
+                            </div>
+                        @else
+                            <p>Your cart is empty.</p>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
