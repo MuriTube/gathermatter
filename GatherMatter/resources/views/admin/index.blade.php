1a8b54334672dc1a2a025ac62dc64615cc67178a
gr01b-bekmez <bekmez-emirhan@outlook.com>
2023-06-20 08:16:19 +0200

    Adminpanel erweitert

--- GatherMatter/resources/views/admin/index.blade.php
+++ GatherMatter/resources/views/admin/index.blade.php
@@ -4,7 +4,39 @@
     <div class="container">
         <h1 class="my-4">Adminpanel</h1>
         <h2 class="mb-4">Usermanagement</h2>
+        
+        <div class="container">
+    <div class="row mb-4">
+        <!-- Suchfeld -->
+        <div class="col-md-6">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span style="height:38px;" class="input-group-text" id="search-addon"><i  class="fas fa-search"></i></span>
+                </div>
+                <input type="text" id="search" placeholder="Search users..." class="form-control" aria-describedby="search-addon">
+            </div>
+        </div>
+    </div>
 
+    <div class="row mb-4">
+        <!-- Dropdown-Menü für Sortieroptionen -->
+        <div class="col-md-6">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span style="height:38px;"  class="input-group-text" id="sort-addon"><i class="fas fa-filter"></i></span>
+                </div>
+                <select id="sort" class="form-control custom-select" aria-describedby="sort-addon">
+                    <option value="">Sort by Username...</option>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    
         @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
@@ -20,7 +52,7 @@
                 <th>Actions</th>
             </tr>
             </thead>
-            <tbody>
+            <tbody id="userTableBody">
             @foreach ($users as $user)
                 <tr>
                     <td>{{ $user->name }}</td>
